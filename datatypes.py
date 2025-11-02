abb03cb97144291d86993e84f4d24abe3471e7bf
Aanchal Mishra <aanchalkviit@gmail.com>
2025-11-02 06:42:58 +0530

    first commit

---
+++ datatypes.py
@@ -0,0 +1,9 @@
+# Checking data types
+
+print(type(10))          # Int
+print(type('Aanchal'))          # String
+print(type([1, 2, 3]))        # List
+print(type({'name':'Aanchal'})) # Dictionary
+print(type(['Aanchal', 'Aaditi', 'Aakashi']))   # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type(4-2j))      # Complex number
