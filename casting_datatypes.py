4ad0a3f1bcab6f1d48dc49267d924ef607d2a6a5
Aanchal Mishra <aanchalkviit@gmail.com>
2025-11-07 14:22:32 +0530

    checking data types and casting

---
+++ casting_datatypes.py
@@ -0,0 +1,30 @@
+#casting: converting one datatype to another datatype
+
+#int to float
+num_int = 10
+y = float(num_int)
+print("y:",y)
+
+#float to int
+gravity = 9.8
+print(int(gravity))
+
+#int to string
+num_int = 10
+print(num_int)
+num_str = str(num_int)
+print(num_str)
+
+#string to int or float
+num_str = '15'
+print(num_str)
+num_int = int(num_str)
+print(num_int)
+num_float = float(num_str)
+print(num_float)
+
+#string to list
+num_str = "Hello World"
+print(num_str)
+num_list = list(num_str)
+print(num_list)
