717ea97efe8f3db21fef0dec3ef8b0680acf5c58
Aanchal Mishra <aanchalkviit@gmail.com>
2025-11-08 17:57:46 +0530

    circumfrence and area of circle exercise

---
+++ exercises/circumfrence_of_circle.py
@@ -0,0 +1,14 @@
+# Find area of a circle
+# Find circumfrence of a circle
+
+r =30
+pi = 3.14
+area_of_circle = 2 * pi * r
+circum_of_circle = pi * r**2;
+print("Area of circle is:", area_of_circle)
+print("Circumfrence of circle is:", circum_of_circle)
+#convert area and circumfrence to int data type
+area_of_circle_int = int(area_of_circle)
+circum_of_circle_int = int(circum_of_circle)
+print(area_of_circle_int);
+print(circum_of_circle_int)
