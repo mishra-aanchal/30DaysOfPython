78537c482880ad4daf0f8f5851981a3b0cf7512a
Aanchal Mishra <aanchalkviit@gmail.com>
2025-11-11 22:39:44 +0530

    operators lesson updated

--- exercises/operators_exercise.py
+++ exercises/operators_exercise.py
@@ -14,3 +14,20 @@ mass = 75
 gravity = 9.81
 weight = mass*gravity
 print('weight of this object is =', weight)
+
+#Exercise
+#Declare your age as integer variable
+age = 25
+print('age:', age)
+#Declare your height as a float variable
+height = 5.51
+print('height:', height)
+#Declare a variable that store a complex number
+complex_num = 1+2j
+print('complex_num:', complex_num)
+
+'''Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle 
+(area = 0.5 x b x h).'''
+
+
+
