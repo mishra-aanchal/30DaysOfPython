36ca0bcf16b60dcadf5c527343a827f49b0871a7
Aanchal Mishra <aanchalkviit@gmail.com>
2025-11-11 22:37:15 +0530

    operators lesson updated

---
+++ exercises/operators_exercise.py
@@ -0,0 +1,16 @@
+#area of circle = pi*r**2
+radius = 10
+area_of_circle = 3.14*10**2
+print('area of this circle =', area_of_circle)
+
+#area of rectangle = l*b
+length = 5
+width = 10
+area_of_rect = length*width
+print('area of this rectangle =', area_of_rect)
+
+#weight of an object = mass*gravity
+mass = 75
+gravity = 9.81
+weight = mass*gravity
+print('weight of this object is =', weight)
--- operators.py
+++ operators.py
@@ -25,3 +25,33 @@ print('a*b =', product)
 print('a/b =', division)
 print('a//b =', floor_division)
 print('a**b =', exponential)
+
+# Comparison Operators
+print(2==3) #False
+print(3!=2) #True
+print(3>2) #True
+print(2<1) #False
+print(3>=2) #True
+print(2<=1) #False
+print(len('avocado') == len('nachos')) #false
+print(len('mumbai') != len('karnal')) #true
+print(len('python') > len('dragon')) 
+
+#additonal comparison Operators 
+print('1 is 1', 1 is 1 )
+print('2 is not 1', 2 is not 1)
+print('A in Aanchal', 'A' in 'Aanchal')
+print('B not in Aanchal', 'B' not in 'Aanchal')
+
+#Logical Operators
+print(3>2 and 5>4)
+print(2>5 and 5>9)
+print(2>1 and 5>9)
+print(2>5 or 5>3)
+print(3>5 or 2>3)
+print('True or False:', True or False)
+print(not 3>2)
+print(not True)
+print(not False)
+
+
