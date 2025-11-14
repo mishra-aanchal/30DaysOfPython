e4defaddc21d681ffb8964499e242ba2b4fb329a
Aanchal Mishra <aanchalkviit@gmail.com>
2025-11-14 23:53:31 +0530

    string intro

---
+++ strings.py
@@ -0,0 +1,14 @@
+#string: Any data type written as text is a string. 
+#Any data under single, double or triple quote are strings.
+message = 'Hello World'
+print(message)
+print(len(message))
+
+#Multiline string is created by using triple single (''') or triple double quotes (""")
+multiline_message = '''I am an educator and enjoy teaching.
+I love building communities, and meeting like minded people.'''
+print(multiline_message)
+#or
+multiline_message = """I am a educator and enjoy teaching.
+I love building communities, and meeting like minded people."""
+print(multiline_message)
