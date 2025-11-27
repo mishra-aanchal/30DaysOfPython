464c0b488769977cc583610263b00592620a1244
Aanchal Mishra <aanchalkviit@gmail.com>
2025-11-27 23:53:22 +0530

    Finding len() of lists

--- exercises/indivuidual_runs.py
+++ exercises/indivuidual_runs.py
@@ -1,6 +1,14 @@
-company = 'AI for All'
-print(company[0])
-print(company.rindex('l'))
-print(company[5])
-print(company.index('A'))
-print(company.rfind('l'))
+fruits = ['Leechee', 'mango', 'apple', 'apple', 'pineapple']
+veggies = ['capcicum','bitterguard', 'onion', 'potato', 'tomato']
+diary_products = ['milk', 'butter', 'meat', 'yogurt']
+languages = ['JS', 'Python', 'java', 'ruby']
+countries = ['Japan', 'Switzerland', 'Italy', 'Norway']
+
+print("Aanchal's fav fruits:", fruits)
+print('Number of fruits:', len(fruits))
+print("Aanchal's fav veggies:", veggies)
+print('Number of veggies:', len(veggies))
+print('Diary Products :', diary_products)
+print('Number of diary products:', len(diary_products))
+print("Aanchal wishes to visit these Countries:", countries)
+print('Number of countries:',len(countries))
--- lists.py
+++ lists.py
@@ -1,1 +1,18 @@
 #A list is collection of different data types which is ordered and modifiable(mutable). A list can be empty or it may have different data type items.
+
+#In python we can create lists in 2 ways:
+#1. using build-in funcn
+list = list()
+empty_list = list() #this is an empty list, no item in the list
+print(len(empty_list)) #0
+
+#2. using []
+list = []
+empty_list = [] #this is an empty list, no item in the list
+print(len(empty_list)) #0
+
+fruits = ['Leechee', 'mango', 'apple', 'apple', 'pineapple']
+veggies = ['capcicum','bitterguard', 'onion', 'potato', 'tomato']
+diary_products = ['milk', 'butter', 'meat', 'yogurt']
+languages = ['JS', 'Python', 'java', 'ruby']
+countries = ['India', 'USA', 'Switzerland', 'Norway']
