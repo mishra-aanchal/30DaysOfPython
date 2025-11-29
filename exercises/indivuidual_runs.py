ea9f9bd76192890189c407a4d69bfb152b2b2588
Aanchal Mishra <aanchalkviit@gmail.com>
2025-11-29 16:35:48 +0530

    List unpacking /destructuring

--- exercises/indivuidual_runs.py
+++ exercises/indivuidual_runs.py
@@ -3,3 +3,7 @@ second_lastveg = veggies[-2]
 print(second_lastveg)
 first_veg = veggies[-5]
 print(first_veg)
+
+Friends = ['Aanchal', 'Hashini', 'Yashi', 'Anwesha', 'Bharvi', 'Ketki']
+first_friend, second_friend, third_friend, *rest = Friends
+print(first_friend)
--- lists.py
+++ lists.py
@@ -50,5 +50,10 @@ veggies = ['capsicum','bitterguard', 'onion', 'potato', 'tomato']
 second_lastveg = veggies[-2]
 print(second_lastveg)
 
+#Unpacking List Items
+Friends = ['Aanchal', 'Hashini', 'Yashi', 'Anwesha', 'Bharvi', 'Ketki']
+first_friend, second_friend, third_friend, *rest = Friends
+print(first_friend)
+
 
 
